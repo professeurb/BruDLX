607447b6db34bd5ac60114476dd68090482e4cef
professeurb <olibrunet@free.fr>
2023-07-28 10:52:49 +0200

    On fait le ménage en ne gardant que la version en C. Par contre, on conserve le code OCaml commenté.

--- lib/dlx.ml
+++ lib/dlx.ml
@@ -41,12 +41,12 @@ and up = 2
 and down = 3
 and data = 4
 
-let insert_horiz tab l i =
-  let r = tab.(l + right) in
-  tab.(l + right) <- i;
-  tab.(i + left) <- l;
-  tab.(r + left) <- i;
-  tab.(i + right) <- r
+(* let insert_horiz tab l i = *)
+(*   let r = tab.(l + right) in *)
+(*   tab.(l + right) <- i; *)
+(*   tab.(i + left) <- l; *)
+(*   tab.(r + left) <- i; *)
+(*   tab.(i + right) <- r *)
 
 let insert_horiz_big tab l i =
   let r = tab.{l + right} |> Int64.to_int in
@@ -55,12 +55,12 @@ let insert_horiz_big tab l i =
   tab.{r + left} <- i |> Int64.of_int;
   tab.{i + right} <- r |> Int64.of_int
 
-let insert_vert tab u i =
-  let d = tab.(u + down) in
-  tab.(u + down) <- i;
-  tab.(i + up) <- u;
-  tab.(i + down) <- d;
-  tab.(d + up) <- i
+(* let insert_vert tab u i = *)
+(*   let d = tab.(u + down) in *)
+(*   tab.(u + down) <- i; *)
+(*   tab.(i + up) <- u; *)
+(*   tab.(i + down) <- d; *)
+(*   tab.(d + up) <- i *)
 
 let insert_vert_big tab u i =
   let d = tab.{u + down} |> Int64.to_int in
@@ -69,104 +69,106 @@ let insert_vert_big tab u i =
   tab.{i + down} <- d |> Int64.of_int;
   tab.{d + up} <- i |> Int64.of_int
 
-let hide_vert tab i =
-  let above = tab.(i + up)
-  and below = tab.(i + down) in
-  tab.(above + down) <- below;
-  tab.(below + up) <- above
+(* let hide_vert tab i = *)
+(*   let above = tab.(i + up) *)
+(*   and below = tab.(i + down) in *)
+(*   tab.(above + down) <- below; *)
+(*   tab.(below + up) <- above *)
 
-let restore_vert tab i =
-  let above = tab.(i + up)
-  and below = tab.(i + down) in
-  tab.(above + down) <- i;
-  tab.(below + up) <- i
+(* let restore_vert tab i = *)
+(*   let above = tab.(i + up) *)
+(*   and below = tab.(i + down) in *)
+(*   tab.(above + down) <- i; *)
+(*   tab.(below + up) <- i *)
 
-let incr_point tab i = tab.(i + data) <- tab.(i + data) + 1
+(* let incr_point_old tab i = *)
+(*   tab.(i + data) <- tab.(i + data) + 1 *)
 
-let incr_point_big tab i =
-  tab.{i + data} <- Int64.add tab.{i + data} 5L
+let incr_point_big incr_point i =
+  incr_point.{i + data} <-
+    Int64.add incr_point.{i + data} 5L
 
-let decr_point tab i = tab.(i + data) <- tab.(i + data) - 1
+(* let decr_point tab i = tab.(i + data) <- tab.(i + data) - 1 *)
 
-let rec cover_col_aux2 tab row pos =
-  if pos <> row then begin
-    hide_vert tab pos;
-    decr_point tab tab.(pos + data);
-    cover_col_aux2 tab row tab.(pos + right)
-  end
+(* let rec cover_col_aux2 tab row pos = *)
+(*   if pos <> row then begin *)
+(*     hide_vert tab pos; *)
+(*     decr_point tab tab.(pos + data); *)
+(*     cover_col_aux2 tab row tab.(pos + right) *)
+(*   end *)
 
-let rec cover_col_aux1 tab col pos =
-  if pos <> col then begin
-    cover_col_aux2 tab pos tab.(pos + right);
-    cover_col_aux1 tab col tab.(pos + down)
-  end
+(* let rec cover_col_aux1 tab col pos = *)
+(*   if pos <> col then begin *)
+(*     cover_col_aux2 tab pos tab.(pos + right); *)
+(*     cover_col_aux1 tab col tab.(pos + down) *)
+(*   end *)
 
-let cover_column tab col =
-  let l = tab.(col + left)
-  and r = tab.(col + right) in
-  tab.(l + right) <- r;
-  tab.(r + left) <- l;
-  cover_col_aux1 tab col tab.(col + down)
+(* let cover_column tab col = *)
+(*   let l = tab.(col + left) *)
+(*   and r = tab.(col + right) in *)
+(*   tab.(l + right) <- r; *)
+(*   tab.(r + left) <- l; *)
+(*   cover_col_aux1 tab col tab.(col + down) *)
 
-let cover_column_weird tab col =
-  let l = tab.(col + left)
-  and r = tab.(col + right) in
-  tab.(l + right) <- r;
-  tab.(r + left) <- l;
-  let prev = tab.(0 + down) in
-  tab.(col + right) <- prev;
-  tab.(0 + down) <- col;
-  cover_col_aux1 tab col tab.(col + down)
+(* let cover_column_weird tab col = *)
+(*   let l = tab.(col + left) *)
+(*   and r = tab.(col + right) in *)
+(*   tab.(l + right) <- r; *)
+(*   tab.(r + left) <- l; *)
+(*   let prev = tab.(0 + down) in *)
+(*   tab.(col + right) <- prev; *)
+(*   tab.(0 + down) <- col; *)
+(*   cover_col_aux1 tab col tab.(col + down) *)
 
-let rec uncover_col_aux2 tab row pos =
-  if pos <> row then begin
-    restore_vert tab pos;
-    incr_point tab tab.(pos + data);
-    uncover_col_aux2 tab row tab.(pos + left)
-  end
+(* let rec uncover_col_aux2 tab row pos = *)
+(*   if pos <> row then begin *)
+(*     restore_vert tab pos; *)
+(*     incr_point_old tab tab.(pos + data); *)
+(*     uncover_col_aux2 tab row tab.(pos + left) *)
+(*   end *)
 
-let rec uncover_col_aux1 tab col pos =
-  if pos <> col then begin
-    uncover_col_aux2 tab pos tab.(pos + left);
-    uncover_col_aux1 tab col tab.(pos + up)
-  end
+(* let rec uncover_col_aux1 tab col pos = *)
+(*   if pos <> col then begin *)
+(*     uncover_col_aux2 tab pos tab.(pos + left); *)
+(*     uncover_col_aux1 tab col tab.(pos + up) *)
+(*   end *)
 
-let rec uncover_col_aux1_weird tab col pos =
-  uncover_col_aux2 tab pos tab.(pos + left);
-  let pos_up = tab.(pos + up) in
-  if pos_up <> col then
-    uncover_col_aux1_weird tab col pos_up
-  else tab.(col + down) <- pos
+(* let rec uncover_col_aux1_weird tab col pos = *)
+(*   uncover_col_aux2 tab pos tab.(pos + left); *)
+(*   let pos_up = tab.(pos + up) in *)
+(*   if pos_up <> col then *)
+(*     uncover_col_aux1_weird tab col pos_up *)
+(*   else tab.(col + down) <- pos *)
 
-let uncover_column tab col =
-  uncover_col_aux1 tab col tab.(col + up);
-  let l = tab.(col + left) in
-  let r = tab.(col + right) in
-  tab.(l + right) <- col;
-  tab.(r + left) <- col
+(* let uncover_column tab col = *)
+(*   uncover_col_aux1 tab col tab.(col + up); *)
+(*   let l = tab.(col + left) in *)
+(*   let r = tab.(col + right) in *)
+(*   tab.(l + right) <- col; *)
+(*   tab.(r + left) <- col *)
 
-let uncover_column_weird tab =
-  let col = tab.(0 + down) in
-  (* assert (col <> 0); *)
-  tab.(0 + down) <- tab.(col + right);
-  uncover_col_aux1_weird tab col tab.(col + up);
-  let l = tab.(col + left) in
-  let r = tab.(l + right) in
-  tab.(l + right) <- col;
-  tab.(r + left) <- col;
-  tab.(col + right) <- r
+(* let uncover_column_weird tab = *)
+(*   let col = tab.(0 + down) in *)
+(*   (* assert (col <> 0); *) *)
+(*   tab.(0 + down) <- tab.(col + right); *)
+(*   uncover_col_aux1_weird tab col tab.(col + up); *)
+(*   let l = tab.(col + left) in *)
+(*   let r = tab.(l + right) in *)
+(*   tab.(l + right) <- col; *)
+(*   tab.(r + left) <- col; *)
+(*   tab.(col + right) <- r *)
 
-let rec cover_right tab row pos =
-  if pos <> row then begin
-    cover_column tab tab.(pos + data);
-    cover_right tab row tab.(pos + right)
-  end
+(* let rec cover_right tab row pos = *)
+(*   if pos <> row then begin *)
+(*     cover_column tab tab.(pos + data); *)
+(*     cover_right tab row tab.(pos + right) *)
+(*   end *)
 
-let rec uncover_left tab row pos =
-  if pos <> row then begin
-    uncover_column tab tab.(pos + data);
-    uncover_left tab row tab.(pos + left)
-  end
+(* let rec uncover_left tab row pos = *)
+(*   if pos <> row then begin *)
+(*     uncover_column tab tab.(pos + data); *)
+(*     uncover_left tab row tab.(pos + left) *)
+(*   end *)
 
 (* let print_info tab = *)
 (*   let rec aux1 col = *)
@@ -180,48 +182,47 @@ let rec uncover_left tab row pos =
 (*   Printf.printf "Row :"; *)
 (*   aux1 tab.(0 + right) *)
 
-let rec select_col tab cand card next =
-  if next = 0 then cand
-  else
-    let new_card = tab.(next + data) in
-    if new_card < card then
-      select_col tab next new_card tab.(next + right)
-    else select_col tab cand card tab.(next + right)
+(* let rec select_col tab cand card next = *)
+(*   if next = 0 then cand *)
+(*   else *)
+(*     let new_card = tab.(next + data) in *)
+(*     if new_card < card then *)
+(*       select_col tab next new_card tab.(next + right) *)
+(*     else select_col tab cand card tab.(next + right) *)
 
-let rec forward tab =
-  (* print_endline "forward"; *)
-  let col = tab.(0 + right) in
-  if col <> 0 then
-    let col =
-      select_col tab col tab.(col + data) tab.(col + right)
-    in
-    let row = tab.(col + down) in
-    if row = col then backward tab
-    else begin
-      cover_column_weird tab col;
-      (* print_info tab; *)
-      cover_right tab row tab.(row + right);
-      forward tab
-    end
+(* let rec forward tab = *)
+(*   let col = tab.(0 + right) in *)
+(*   if col <> 0 then *)
+(*     let col = *)
+(*       select_col tab col tab.(col + data) tab.(col + right) *)
+(*     in *)
+(*     let row = tab.(col + down) in *)
+(*     if row = col then backward tab *)
+(*     else begin *)
+(*       cover_column_weird tab col; *)
+(*       (* print_info tab; *) *)
+(*       cover_right tab row tab.(row + right); *)
+(*       forward tab *)
+(*     end *)
 
-and backward tab =
-  (* print_endline "backward"; *)
-  (* print_info tab; *)
-  let col = tab.(0 + down) in
-  if col <> 0 then begin
-    let row = tab.(col + down) in
-    uncover_left tab row tab.(row + left);
-    let next_row = tab.(row + down) in
-    if next_row <> col then begin
-      tab.(col + down) <- next_row;
-      cover_right tab next_row tab.(next_row + right);
-      forward tab
-    end
-    else begin
-      uncover_column_weird tab;
-      backward tab
-    end
-  end
+(* and backward tab = *)
+(*   (* print_endline "backward"; *) *)
+(*   (* print_info tab; *) *)
+(*   let col = tab.(0 + down) in *)
+(*   if col <> 0 then begin *)
+(*     let row = tab.(col + down) in *)
+(*     uncover_left tab row tab.(row + left); *)
+(*     let next_row = tab.(row + down) in *)
+(*     if next_row <> col then begin *)
+(*       tab.(col + down) <- next_row; *)
+(*       cover_right tab next_row tab.(next_row + right); *)
+(*       forward tab *)
+(*     end *)
+(*     else begin *)
+(*       uncover_column_weird tab; *)
+(*       backward tab *)
+(*     end *)
+(*   end *)
 
 type 'a t = {
   mutable counter : int;
@@ -264,29 +265,29 @@ let add_shape : 'a t -> 'a list -> unit =
     t.counter <- t.counter + (5 * List.length shape)
   end
 
-let compile : 'a t -> int array =
- fun t ->
-  let arr = Array.init t.counter (fun i -> i / 5 * 5) in
-  Hashtbl.iter
-    (fun _ (is_primary, pos) ->
-      arr.(pos + data) <- 0;
-      if is_primary then insert_horiz arr 0 pos)
-    t.item_tbl;
-  Hashtbl.iter
-    (fun pos item_list ->
-      let curr_pos = ref pos in
-      List.iter
-        (fun item ->
-          let _, item_pos = Hashtbl.find t.item_tbl item in
-          if !curr_pos <> pos then
-            insert_horiz arr pos !curr_pos;
-          insert_vert arr item_pos !curr_pos;
-          incr_point arr item_pos;
-          arr.(!curr_pos + data) <- item_pos;
-          curr_pos := 5 + !curr_pos)
-        item_list)
-    t.shape_tbl;
-  arr
+(* let compile : 'a t -> int array = *)
+(*  fun t -> *)
+(*   let arr = Array.init t.counter (fun i -> i / 5 * 5) in *)
+(*   Hashtbl.iter *)
+(*     (fun _ (is_primary, pos) -> *)
+(*       arr.(pos + data) <- 0; *)
+(*       if is_primary then insert_horiz arr 0 pos) *)
+(*     t.item_tbl; *)
+(*   Hashtbl.iter *)
+(*     (fun pos item_list -> *)
+(*       let curr_pos = ref pos in *)
+(*       List.iter *)
+(*         (fun item -> *)
+(*           let _, item_pos = Hashtbl.find t.item_tbl item in *)
+(*           if !curr_pos <> pos then *)
+(*             insert_horiz arr pos !curr_pos; *)
+(*           insert_vert arr item_pos !curr_pos; *)
+(*           incr_point_old arr item_pos; *)
+(*           arr.(!curr_pos + data) <- item_pos; *)
+(*           curr_pos := 5 + !curr_pos) *)
+(*         item_list) *)
+(*     t.shape_tbl; *)
+(*   arr *)
 
 let compile_bigarray :
     'a t ->
@@ -320,14 +321,14 @@ let compile_bigarray :
     t.shape_tbl;
   arr
 
-let has_solution_comp arr =
-  forward arr;
-  arr.(0 + down) <> 0
+(* let has_solution_comp arr = *)
+(*   forward arr; *)
+(*   arr.(0 + down) <> 0 *)
 
-let has_solution pb =
-  let arr = compile pb in
-  forward arr;
-  arr.(0 + down) <> 0
+(* let has_solution pb = *)
+(*   let arr = compile pb in *)
+(*   forward arr; *)
+(*   arr.(0 + down) <> 0 *)
 
 (* let count_solutions pb = *)
 (*   let arr = compile pb *)
--- lib/dlx.mli
+++ lib/dlx.mli
@@ -4,7 +4,7 @@ val init : unit -> 'a t
 val add_primary : 'a t -> 'a -> unit
 val add_secondary : 'a t -> 'a -> unit
 val add_shape : 'a t -> 'a list -> unit
-val compile : 'a t -> int array
+(* val compile : 'a t -> int array *)
 
 val compile_bigarray :
   'a t ->
@@ -13,13 +13,13 @@ val compile_bigarray :
     Bigarray.c_layout )
   Bigarray.Array1.t
 
-val has_solution_comp : int array -> bool
-val has_solution : 'a t -> bool
+(* val has_solution_comp : int array -> bool *)
+(* val has_solution : 'a t -> bool *)
 val count_solutions : 'a t -> int
 
 (* val count_solutions_c : 'a t -> int *)
-val forward : int array -> unit
-val backward : int array -> unit
+(* val forward : int array -> unit *)
+(* val backward : int array -> unit *)
 
 val forward_c :
   ( int64,
